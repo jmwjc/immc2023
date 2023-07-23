daa1d128545804cb66b85600c8a958411a0e366a
WuJC <jcwu@hqu.edu.cn>
2023-07-23 11:32:27 +0800

    revise Q2

--- Q2.jl
+++ Q2.jl
@@ -1,4 +1,4 @@
-using TextAnalysis, CategoricalArrays
+using StringAnalysis, CategoricalArrays, Languages, XLSX
 
 include("import.jl")
 
@@ -6,17 +6,25 @@ df = import_xlsx("Appendix III.xlsx","Sheet2")
 
 asin = levels(categorical(df[!,"asin"]))
 
-for a in asin[1:1]
+# for a in asin[1:1]
+a = asin[1]
     dft = filter(:asin=> x-> x == a, df)
-    sds = TextAnalysis.StringDocument{String}[]
-    for r in df[!,"reviewText"]
-        sd = StringDocument(lowercase(r))
-        prepare!(sd, strip_articles|strip_prepositions|strip_pronouns|strip_stopwords)
-        prepare!(sd, strip_numbers|strip_html_tags|strip_non_letters)
-        prepare!(sd, strip_articles|strip_prepositions|strip_pronouns|strip_stopwords)
-        push!(sds,sd)
+    sd = StringAnalysis.AbstractDocument[]
+    for r in dft[:,"reviewText"]
+        t = " "
+        for w in eachmatch(r"[a-zA-Z]+",r)
+            t *= w.match*" "
+        end
+        # s = StringDocument(lowercase(t))
+        # println(text(s))
+        # println(stem!(s))
+        push!(sd,NGramDocument(lowercase(t)))
     end
-    crps = Corpus(sds)
-    println(lsa(crps))
-    # println(dft)
-end
+    crps = Corpus(sd)
+    # prepare!(crps, strip_articles|strip_prepositions|strip_pronouns|strip_stopwords|stem_words)
+    prepare!(crps, strip_articles|strip_prepositions|strip_pronouns|strip_stopwords)
+    # println(dft[:,"reviewText"][1])
+    # println(text(crps.documents[1]))
+    update_lexicon!(crps)
+
+    rstrip(word,['s'])
