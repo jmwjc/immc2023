d21d8bef94de50871690492a3b173710c832302e
WuJC <jcwu@hqu.edu.cn>
2023-07-26 18:34:01 +0800

    add Q4

--- Q4.jl
+++ Q4.jl
@@ -1,21 +1,33 @@
-using TextAnalysis
+using TextAnalysis, Distances
 
-function is_automated_review(review_text::AbstractString, existing_reviews::Vector{AbstractString}; similarity_threshold::Float64=0.8, ngram_range::Tuple{Int,Int}=(1,1))
+function is_automated_review(review_text::AbstractString, existing_reviews::Vector{T}; similarity_threshold::Float64=0.8, ngram_range::Tuple{Int,Int}=(1,1)) where T<:AbstractString
     # Tokenize the review text and existing reviews
-    review_tokens = tokenize(review_text)
-    existing_tokens = [tokenize(existing_review) for existing_review in existing_reviews]
+    review_tokens = TokenDocument(review_text)
+    existing_tokens = [TokenDocument(existing_review) for existing_review in existing_reviews]
+    review_tokens = Corpus([review_tokens])
+    existing_tokens = Corpus(existing_tokens)
+    update_lexicon!(review_tokens)
+    update_lexicon!(existing_tokens)
     
     # Create a document-term matrix for the existing reviews
-    existing_dtm = DocumentTermMatrix(existing_tokens, ngram_range=ngram_range)
+    # existing_dtm = DocumentTermMatrix(crps, ngram_range=ngram_range)
+    existing_dtm = DocumentTermMatrix(existing_tokens)
     
     # Create a document-term matrix for the current review
-    review_dtm = DocumentTermMatrix([review_tokens], ngram_range=ngram_range)
-    
+    # review_dtm = DocumentTermMatrix([review_tokens], ngram_range=ngram_range)
+    review_dtm = DocumentTermMatrix(review_tokens)
+
+    # normalize!(existing_dtm)
+    # normalize!(review_dtm)
+
+    similarity = Distances.cosine(existing_dtm, review_dtm)
+
     # Compute the cosine similarity between the current review and the existing reviews
-    similarities = [cosine_similarities(review_dtm, existing_dtm)[1, i] for i in 1:size(existing_dtm)[1]]
+    println(similarity)
+    # similarities = [cosine_similarities(review_dtm, existing_dtm)[1, i] for i in 1:size(existing_dtm)[1]]
     
     # Check if the maximum similarity is greater than the threshold
-    return maximum(similarities) > similarity_threshold
+    # return maximum(similarities) > similarity_threshold
 end
 existing_reviews = [
     "Great product, I love it!",
